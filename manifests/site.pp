bf19e24f94338989b131d84ec606edbdff4dce03
Aliaksei <45315992+AlTriV@users.noreply.github.com>
2023-01-03 22:31:08 +0300

    Add master.puppet node configuration

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,15 @@
-node default{
-  file{ '/root/README':
+node master.puppet {
+  file { '/etc/puppetlabs/puppet/autosign.conf':
     ensure => file,
     content => "Test",    
+  }
+  
+  firewalld_custom_service { 'puppet':
+    short => 'puppet',
+    description => 'Puppet Client access Puppet Server',
+	port => {
+            'port'     => '8140',
+            'protocol' => 'tcp',
+    },
   }
 }
